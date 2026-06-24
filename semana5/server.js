086e9c76150af3a4b061a7e7b7b34514d98546cf
vegapaula399-code <vegapaula399@gmail.com>
2026-06-24 17:40:56 +0000

    Actividad semana 5

---
+++ semana5/server.js
@@ -0,0 +1,35 @@
+
+http://localhost:3000
+http://localhost:3000/saludo
+http://localhost:3000/mensaje/juan
+
+
+
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Servidor activo');
+});
+
+app.get('/saludo', (req, res) => {
+  res.send('Hola comunidad');
+});
+
+app.get('/mensaje/:nombre', (req, res) => {
+  const nombre = req.params.nombre;
+  res.send('Hola ' + nombre);
+});
+
+app.listen(3000, () => {
+  console.log('Servidor ejecutándose en puerto 3000');
+});
+
+
+httptouch reflexion-semana5.txt
+
+{
+  "mensaje": "Reporte de prueba enviado por la comunidad"
+}
